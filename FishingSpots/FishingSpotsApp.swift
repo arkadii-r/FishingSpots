3c4d343c94e49088fad6de7d24566e76cc1994b6
Arkadii Ratevosian <jargkee@gmail.com>
2026-03-19 19:22:58 +0300

    Initial Commit

---
+++ FishingSpots/FishingSpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishingSpotsApp.swift
+//  FishingSpots
+//
+//  Created by Ратевосян Аркадий Владимирович on 19.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FishingSpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
